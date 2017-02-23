5c7cda4d045b3c5fd9cb00cf183b189961ca0c6a
Justin Lane <justin.l.lane@gmail.com>
2017-02-22 21:30:59 -0700

    20170222 2130 - Import function

--- app/component/unit.main.js
+++ app/component/unit.main.js
@@ -1,14 +1,39 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // Mobius Main Unit Component
 ////////////////////////////////////////////////////////////////////////////////////////////////
+
+mobiusEngine.unit = {};
+
+mobiusEngine.unit.controller = function() {
+	this.states = {
+		controls: false
+	}
+
+	this.import = undefined;
+
+	this.units = {};
+
+	this.toggleState = function(key) { this.states[key] = !this.states[key]; };
+
+	this.onImport = function() {
+		var imp = JSON.parse(this.import);
+		if(_.isArray(this.import)) {
+			// Import an array of units
+		}
+		else if(_.isObject(this.import)) {
+			// Import a single unit
+		}
+		else {
+			// Not sure what is being imported but it is not expected.
+			console.log("Unexpected parameter type passed to onImport.  Expected array or object but was passed " + typeof(this.import));
+		}
+	};
+};
+
 mobiusEngine.app.component("unitMain",{
 	templateUrl: 'app/component/unit.main.html',
-	controller: function() {
-		this.states = {
-			controls: false
-		};
-
-		this.toggleState = function(key) { this.states[key] = !this.states[key]; };
-	},
-	bindings: {}
+	controller: mobiusEngine.unit.controller,
+	bindings: {
+		"units": "="
+	}
 });
