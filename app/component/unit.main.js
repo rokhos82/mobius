0a6c22d5f7024181c00e2361d6408c824a9709a8
Justin Lane <justin.l.lane@gmail.com>
2017-02-27 08:35:58 -0700

    20170227 0835 - Unit card info and style

--- app/component/unit.main.js
+++ app/component/unit.main.js
@@ -4,6 +4,38 @@
 
 mobiusEngine.unit = {};
 
+mobiusEngine.unit.calculateFirepower = function(unit) {
+	var fp = {
+		"direct": 0,
+		"packet": 0
+	};
+
+	var _map = function(weapon) {
+		var v = 0;
+		
+		if(_.isNumber(weapon.volley)) {
+			v = weapon.volley;
+		}
+		else {
+			_.each(weapon.volley,function(x){ v += x; });
+		}
+		
+		return weapon.batteries * weapon.guns * v;
+	};
+	var _pmap = function(weapon) {
+		return weapon.packets * weapon.volley;
+	};
+	var _reduce = function(memo,num) { return memo + num; };
+	
+	var direct = unit["direct-fire"];
+	fp.direct = _.chain(direct).map(_map).reduce(_reduce,0).value();
+
+	var packet = unit["packet-fire"];
+	fp.packet = _.chain(packet).map(_pmap).reduce(_reduce,0).value();
+
+	return fp;
+};
+
 mobiusEngine.unit.validate = function(obj) {
 	var valid = true;
 
@@ -26,6 +58,7 @@ mobiusEngine.unit.controller = function($scope,_data) {
 	this.units = _data.getAllUnits();
 
 	this.getUnit = _data.getUnit;
+	this.firepower = mobiusEngine.unit.calculateFirepower;
 
 	this.toggleState = function(key) { this.states[key] = !this.states[key]; };
 
