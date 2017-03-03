264d032e3b32292efb27f90b60415a125862fb63
Justin Lane <justin.l.lane@gmail.com>
2017-03-03 15:04:36 -0700

    20170303 1504 - Unit card component

---
+++ app/component/card.components.js
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Mobius Card Components
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Unit Card ///////////////////////////////////////////////////////////////////////////////////////
+mobiusEngine.unit.cardController = function($scope) {
+};
+
+mobiusEngine.app.component("unitCard",{
+	template: '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><div class="panel ng-class:{\'panel-info\':hover,\'panel-primary\':!hover}" ng-mouseenter="hover = true" ng-mouseleave="hover = false"><div class="panel-heading mobius-unit-header"><a ui-sref="unitDtl({uuid:$ctrl.unit.uuid})">{{$ctrl.unit.general.name}}</a></div><table class="tabel table-striped table hover"><tbody><tr><td class="mobius-unit-stat col-md-6">Type:</td><td class="mobius-unit-stat">{{$ctrl.unit.general.type}}</td></tr><tr><td class="mobius-unit-stat col-md-6">Size:</td><td class="mobius-unit-stat">{{$ctrl.unit.general.size}}</td></tr><tr><td class="mobius-unit-stat col-md-6">Hull:</td><td class="mobius-unit-stat">{{$ctrl.unit.hull.max}}</td></tr><tr><td class="mobius-unit-stat col-md-6">Shield:</td><td class="mobius-unit-stat">{{$ctrl.unit.shield.max}}</td></tr><tr><td class="mobius-unit-stat col-md-6">Firepower:</td><td class="mobius-unit-stat">{{$ctrl.unit.general.firepower}}</td></tr></tbody></table><div class="panel-footer mobius-unit-footer"><a ui-sref="unitDtl({uuid:$ctrl.unit.uuid})">{{$ctrl.unit.uuid}}</a></div></div></div>',
+	controller: ["$scope","mobius.data.unit",mobiusEngine.unit.cardController],
+	bindings: {
+		unit: "<"
+	}
+});
