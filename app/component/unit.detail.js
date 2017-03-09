b90bc8ac9d5052068dc242e20c0fd668e010ee65
Justin Lane <justin.l.lane@gmail.com>
2017-03-09 13:22:42 -0700

    Buttons work!

--- app/component/unit.detail.js
+++ app/component/unit.detail.js
@@ -6,8 +6,23 @@ mobiusEngine.unit.dtlController = function($scope,_data) {
 	var $ctrl = this;
 	$ctrl.unit = _data.getUnit($ctrl.uuid);
 	$ctrl.link = "http://rokhos82.github.io/mobius/#/unit/import/" + btoa(angular.toJson($ctrl.unit));
+
 	$ctrl.sectionFilter = function(value,index,arr) {
 		return _.omit($ctrl.unit,["direct-fire","packet-fire","uuid"]);
+	};
+
+	$ctrl.addAttribute = function(section,attribute) {
+		var key = attribute.toLowerCase();
+		section[key] = 0;
+	};
+
+	$ctrl.addSection = function(section) {
+		var key = section.toLowerCase();
+		$ctrl.unit[key] = {};
+	};
+
+	$ctrl.removeAttribute = function(section,key) {
+		delete section[key];
 	};
 };
 
