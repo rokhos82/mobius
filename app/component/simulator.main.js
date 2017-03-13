64d54d3576398830f1990cfefc7521cfd8c5f524
Justin Lane <justin.l.lane@gmail.com>
2017-03-13 11:45:43 -0600

    UI Mockup is finished

--- app/component/simulator.main.js
+++ app/component/simulator.main.js
@@ -2,12 +2,35 @@
 // Mobius Simulator Main UI
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 mobiusEngine.simulator = {};
+mobiusEngine.simulator.states = {
+	"setup": "setup",
+	"ready": "ready",
+	"start": "start",
+	"stop": "stop"
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Unit Card ///////////////////////////////////////////////////////////////////////////////////////
 mobiusEngine.simulator.controller = function($scope) {
 	var $ctrl = this;
 
 	$ctrl.alerts = [];
+	$ctrl.state = undefined;
+
+	$ctrl.start = function() {
+		$ctrl.alerts.unshift(new mobiusEngine.pageAlerts.alert("Starting the simulator!","success",3500));
+	};
+
+	$ctrl.dump = function() {
+		$ctrl.alerts.unshift(new mobiusEngine.pageAlerts.alert("Successfully dumped data to console log.","info",2000));
+	};
+
+	$ctrl.$onInit = function() {
+		$ctrl.state = mobiusEngine.simulator.states.setup;
+	};
 };
 
 mobiusEngine.app.component("simulatorMain",{
