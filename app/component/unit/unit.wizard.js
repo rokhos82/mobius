c39854622042a2e9bdb4bc2672e9981063c25b8c
Justin Lane <justin.l.lane@gmail.com>
2017-05-07 15:30:13 -0600

    Adding some validation and tooltips.

--- app/component/unit/unit.wizard.js
+++ app/component/unit/unit.wizard.js
@@ -4,15 +4,52 @@ mobius.unit.wizard.ctrl = function($scope) {
 	var $ctrl = this;
 
 	$ctrl.unit = {
-		general: {},
+		general: {
+			size: undefined
+		},
 		defenses: {},
 		weapons: {},
 		other: {}
 	};
 
+	// Contraints to make sure the unit is valid
+	$ctrl.constraints = {};
+	$ctrl.constraints.general = {
+		size: {
+			placeholder: 6,
+			max: 20
+		}
+	};
+	$ctrl.constraints.defenses = {
+		armor: {
+			placeholder: $ctrl.constraints.general.size.placeholder * 0.5,
+			max: $ctrl.unit.general.size * 0.5
+		}
+	};
+
 	$ctrl.$onInit = function() {
 		$ctrl.options = $ctrl.resolve.options;
 	};
+
+	$ctrl.sizeUpdate = function() {
+		$ctrl.constraints.defenses.armor.max = $ctrl.unit.general.size * 0.5;
+	};
+
+	$ctrl.updateArmor = function() {
+		var defenses = $ctrl.unit.defenses;
+		var max = $ctrl.constraints.defenses.armor.max;
+		if(defenses.armor > max) {
+			// Reset the armor to the max
+			defenses.armor = max;
+		}
+		else if(defenses.armor < 0) {
+			// Reset the armor to zero since negative armor makes no sense.
+			defenses.armor = 0;
+		}
+	};
+
+	// Setup $watch functions for different parts of the unit object
+	$scope.$watch("$ctrl.unit.general.size",$ctrl.sizeUpdate,true);
 };
 
 mobius.app.component("unitWizard",{
