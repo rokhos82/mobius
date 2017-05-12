f94f30e1a39340644d443e569298ef05e174ce96
Justin Lane <justin.l.lane@gmail.com>
2017-05-11 22:56:08 -0600

    Expanding form components.

---
+++ app/component/form.component.js
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Mobius Form Component
+////////////////////////////////////////////////////////////////////////////////////////////////////
+mobius.input = {};
+mobius.input.types = {
+  text: "text"
+};
+
+var mobiusInputCtrl = function($scope) {
+  var $ctrl = this;
+
+  $ctrl.types = mobius.input.types;
+};
+
+mobius.app.component("mobiusInput"),{
+  templateUrl: "app/component/form.component.html",
+  controller: ["$scope",mobiusInputCtrl],
+  bindings: {
+    type: "&",
+    id: "&",
+    label: "&"
+  }
+});
