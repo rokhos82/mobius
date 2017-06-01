d51730de5002ab57d5fde64a31e7c5286df4f3b5
Justin Lane <justin.l.lane@gmail.com>
2017-06-01 17:04:37 -0600

    Sanitized the incoming data rather than the displayed data.

--- app/component/main/main.welcome.js
+++ app/component/main/main.welcome.js
@@ -6,14 +6,19 @@ mobiusEngine.app.component("mainWelcome",{
 	controller: ["$http","$sanitize",function($http,$sanitize){
 		var $ctrl = this;
 		$ctrl.$sanitize = $sanitize;
-		$ctrl.welcome = "Loading please wait...";
+		$ctrl.welcome = ["Loading please wait..."];
 
 		$http({
 			method: "GET",
 			url: "rest/settings.php"
 		}).then(
 			function s(response) {
-				$ctrl.welcome = response.data.general.welcome.slice();
+				var general = response.data.general;
+				$ctrl.welcome = [];
+				for(var i in general.welcome) {
+					var m = general.welcome[i];
+					$ctrl.welcome.push($sanitize(m));
+				}
 			},
 			function f(response) {
 				$ctrl.welcome = "Unable to retrieve data!";
