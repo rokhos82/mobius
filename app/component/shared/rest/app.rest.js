6006e5426033160ae0313b8c767aae91fe5186f2
Justin Lane <justin.l.lane@gmail.com>
2017-06-09 16:53:49 -0600

    Working on $q service interaction

--- app/component/shared/rest/app.rest.js
+++ app/component/shared/rest/app.rest.js
@@ -2,8 +2,8 @@ mobius.rest = {};
 
 mobius.rest.service = mobius.app.factory('mobius.rest',["$http",function($http){
   var _svc = {};
-  _svc.settings = {};
-  return function(options) {
-    console.log(options);
-  };
+
+  $q;
+
+  return _svc;
 }]);
