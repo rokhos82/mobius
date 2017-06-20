142ddb343f7aa8c28c53045e13bad0b3e76c039c
Justin Lane <justin.l.lane@gmail.com>
2017-06-20 17:03:56 -0600

    Adding tech definitions to REST

--- rest/science.php
+++ rest/science.php
@@ -1,2 +1,10 @@
 <?php
+$projects = array(
+  "R&D" => array(
+    "R&D Tech" => array(
+      "Name" => "R&D Tech",
+      "Description" => "Each level of R&D Tech increases the races global reseach bonus by +20.  There are no limits to the number of levels that can be researched."
+    )
+  )
+);
 ?>
