c4cdcfd1252d2eae5fa717a985b44ab772023824
Justin Lane <justin.l.lane@gmail.com>
2017-08-30 17:04:08 -0600

    Game creation progress.

--- app/dashboard/game.component.js
+++ app/dashboard/game.component.js
@@ -17,26 +17,35 @@
 
   NewGameController.$inject = [
     "$window",
+    "app.core.game",
+    "app.core.game.const.status",
     "block.user-login.service"
   ];
 
   /* @ngInject */
-  function NewGameController($window,$user) {
+  function NewGameController(
+    $window,
+    $game,
+    $statuses,
+    $user
+  ) {
     var $ctrl = this;
 
     $ctrl.$onInit = activate;
 
     $ctrl.onCreateGame = onCreateGame;
+    $ctrl.statuses = $statuses;
 
     function activate() {
       $ctrl.session = $user.getSession();
 
       $ctrl.game = {
-        status: 'open'
+        status: $ctrl.statuses.open
       };
     }
 
     function onCreateGame() {
+      $game.create();
     }
   }
 })();
--- app/services/game.service.js
+++ app/services/game.service.js
@@ -3,19 +3,54 @@
 
   angular
     .module('app.core')
-    .service('app.core.game', GameService);
+    .service('app.core.game', GameService)
+    .config(getConstants);
 
   GameService.$inject = [
-    'block.alerts.alertFactory'
+    '$window',
+    'block.alerts.alertFactory',
   ];
 
   /* @ngInject */
   function GameService(
+    $window,
     $alerts
   ) {
+    this.create = create;
     this.save = save;
+
+    var _template = {
+      general: {
+        name: '',
+        description: '',
+        tags: [],
+        status: 0
+      },
+      races: {},
+      players: {}
+    };
+
+    function create(options) {
+      var game = $window.angular.copy(_template);
+
+      game.general.name = options.general.name;
+      game.general.description = options.general.description;
+      game.general.tags;
+
+      $alerts.create('Game Created!','success',5000);
+    }
 
     function save() {
     }
+  }
+
+  getConstants.$inject = [
+    '$provide'
+  ];
+
+  function getConstants(
+    $provide
+  ) {
+    $provide.constant('app.core.game.const.status',{open:'open',closed:'closed'});
   }
 })();
